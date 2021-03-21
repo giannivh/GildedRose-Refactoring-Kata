d018cf6c44e2f7948e4c59b99b0f651ea384f80e
Gianni Van Hoecke <gianni@giannivanhoecke.com>
2021-03-21 15:08:38 +0100

    :white_check_mark: Add unit tests based on requirements

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -9,20 +9,161 @@ import static org.hamcrest.CoreMatchers.is;
 class GildedRoseTest {
 
     @Test
-    void updateQualityLowersSellInAndQualityForNormalItems() {
+    void updateQualityLowersSellInAndQualityByOneForNormalItems() {
         // given
-        String itemName = "Normal item";
-        int itemSellIn  = 5;
-        int itemQuality = 5;
-        Item item = new Item(itemName, itemSellIn, itemQuality);
+        String itemName = "Normal Item";
+        int itemSellIn  = 3;
+        int itemQuality = 3;
+        Item item = createNormalItem(itemSellIn, itemQuality);
 
         // when
         runGildedRoseApp(item);
 
         // then
         assertThat(item.name,    is(equalTo(itemName)));
-        assertThat(item.sellIn,  is(equalTo(4)));
-        assertThat(item.quality, is(equalTo(4)));
+        assertThat(item.sellIn,  is(equalTo(itemSellIn - 1)));
+        assertThat(item.quality, is(equalTo(itemQuality - 1)));
+    }
+
+    @Test
+    void updateQualityLowersQualityTwiceAsFastForExpiredItems() {
+        // given
+        int itemSellIn  = 0;
+        int itemQuality = 3;
+        Item item = createNormalItem(itemSellIn, itemQuality);
+
+        // when
+        runGildedRoseApp(item);
+
+        // then
+        assertThat(item.quality, is(equalTo(itemQuality - 2)));
+    }
+
+    @Test
+    void updateQualityNeverReturnsNegativeQuality() {
+        // given
+        int itemSellIn  = 0;
+        int itemQuality = 0;
+        Item item = createNormalItem(itemSellIn, itemQuality);
+
+        // when
+        runGildedRoseApp(item);
+
+        // then
+        assertThat(item.quality, is(equalTo(0)));
+    }
+
+    @Test
+    void updateQualityIncreasesQualityByOneForAgedBrieItems() {
+        // given
+        int itemSellIn  = 3;
+        int itemQuality = 3;
+        Item item = createAgedBrieItem(itemSellIn, itemQuality);
+
+        // when
+        runGildedRoseApp(item);
+
+        // then
+        assertThat(item.quality, is(equalTo(itemQuality + 1)));
+    }
+
+    @Test
+    void updateQualityIncreasesQualityTwiceAsFastForExpiredAgedBrieItems() {
+        // given
+        int itemSellIn  = 0;
+        int itemQuality = 3;
+        Item item = createAgedBrieItem(itemSellIn, itemQuality);
+
+        // when
+        runGildedRoseApp(item);
+
+        // then
+        assertThat(item.quality, is(equalTo(itemQuality + 2)));
+    }
+
+    @Test
+    void updateQualityNeverReturnsQualityAboveFifty() {
+        // given
+        int itemSellIn  = 0;
+        int itemQuality = 50;
+        Item item = createAgedBrieItem(itemSellIn, itemQuality);
+
+        // when
+        runGildedRoseApp(item);
+
+        // then
+        assertThat(item.quality, is(equalTo(50)));
+    }
+
+    @Test
+    void updateQualityNeverSellsOrDecreasesQualityForLegendaryItems() {
+        // given
+        int itemSellIn  = 3;
+        int itemQuality = 3;
+        Item item = createLegendaryItem(itemSellIn, itemQuality);
+
+        // when
+        runGildedRoseApp(item);
+
+        // then
+        assertThat(item.sellIn,  is(equalTo(itemSellIn)));
+        assertThat(item.quality, is(equalTo(itemQuality)));
+    }
+
+    @Test
+    void updateQualityIncreasesQualityByOneForBackstagePassesItemsWhenSellInIsOverTenDays() {
+        // given
+        int itemSellIn  = 11;
+        int itemQuality = 3;
+        Item item = createBackstagePassesItem(itemSellIn, itemQuality);
+
+        // when
+        runGildedRoseApp(item);
+
+        // then
+        assertThat(item.quality, is(equalTo(itemQuality + 1)));
+    }
+
+    @Test
+    void updateQualityIncreasesQualityByTwoForBackstagePassesItemsWhenSellInIsTenDaysOrLess() {
+        // given
+        int itemSellIn  = 10;
+        int itemQuality = 3;
+        Item item = createBackstagePassesItem(itemSellIn, itemQuality);
+
+        // when
+        runGildedRoseApp(item);
+
+        // then
+        assertThat(item.quality, is(equalTo(itemQuality + 2)));
+    }
+
+    @Test
+    void updateQualityIncreasesQualityByThreeForBackstagePassesItemsWhenSellInIsFiveDaysOrLess() {
+        // given
+        int itemSellIn  = 5;
+        int itemQuality = 3;
+        Item item = createBackstagePassesItem(itemSellIn, itemQuality);
+
+        // when
+        runGildedRoseApp(item);
+
+        // then
+        assertThat(item.quality, is(equalTo(itemQuality + 3)));
+    }
+
+    @Test
+    void updateQualityDropsQualityToZeroForBackstagePassesItemsWhenSellInIsAfterConcert() {
+        // given
+        int itemSellIn  = 0;
+        int itemQuality = 3;
+        Item item = createBackstagePassesItem(itemSellIn, itemQuality);
+
+        // when
+        runGildedRoseApp(item);
+
+        // then
+        assertThat(item.quality, is(equalTo(0)));
     }
 
     // util
@@ -30,5 +171,21 @@ class GildedRoseTest {
     private void runGildedRoseApp(Item... items) {
         GildedRose app = new GildedRose(items);
         app.updateQuality();
+    }
+
+    private Item createNormalItem(int sellIn, int quality) {
+        return new Item("Normal Item", sellIn, quality);
+    }
+
+    private Item createAgedBrieItem(int sellIn, int quality) {
+        return new Item("Aged Brie", sellIn, quality);
+    }
+
+    private Item createLegendaryItem(int sellIn, int quality) {
+        return new Item("Sulfuras, Hand of Ragnaros", sellIn, quality);
+    }
+
+    private Item createBackstagePassesItem(int sellIn, int quality) {
+        return new Item("Backstage passes to a TAFKAL80ETC concert", sellIn, quality);
     }
 }
