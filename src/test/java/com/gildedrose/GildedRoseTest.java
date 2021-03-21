9f6e453989830adea4958f1a5061c1d5515a4622
Gianni Van Hoecke <gianni@giannivanhoecke.com>
2021-03-21 14:38:36 +0100

    :white_check_mark: Introduce first test for sellIn and quality
    As given by the requirements, all normal items lower its sellIn and
    quality by 1

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -2,16 +2,33 @@ package com.gildedrose;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
 
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("foo", app.items[0].name);
+    void updateQualityLowersSellInAndQualityForNormalItems() {
+        // given
+        String itemName = "Normal item";
+        int itemSellIn  = 5;
+        int itemQuality = 5;
+        Item item = new Item(itemName, itemSellIn, itemQuality);
+
+        // when
+        runGildedRoseApp(item);
+
+        // then
+        assertThat(item.name,    is(equalTo(itemName)));
+        assertThat(item.sellIn,  is(equalTo(4)));
+        assertThat(item.quality, is(equalTo(4)));
     }
 
+    // util
+
+    private void runGildedRoseApp(Item... items) {
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+    }
 }
